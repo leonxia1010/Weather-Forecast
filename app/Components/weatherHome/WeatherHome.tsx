fb54abedab3f44d9cb31032b76c3e6fd061c0eff
Leon Xia <huachi1990@hotmail.com>
2025-06-12 10:51:11 +1000

    Feat/use API data instead of mock data

--- app/Components/weatherHome/WeatherHome.tsx
+++ app/Components/weatherHome/WeatherHome.tsx
@@ -1,3 +1,4 @@
+import { fromUnixTime, format } from 'date-fns';
 import {
   WiDaySunny,
   WiCloud,
@@ -6,11 +7,47 @@ import {
   WiDust,
   WiRaindrop,
 } from 'react-icons/wi';
+import { useState, useEffect } from 'react';
+import {
+  getCurrentCityForecastData,
+  type OpenWeatherForecastResponse,
+} from '../../api/openWeatherApi.ts';
+import { Bounce, toast } from 'react-toastify';
+
+export interface MappedWeatherData {
+  currentCity: {
+    city: string;
+  };
+  forecast: ForecastDisplayItem[];
+}
+
+export interface ForecastDisplayItem {
+  day: string; // e.g. "Mon"
+  date: string; // e.g. "23 July"
+  dateLong: string; // e.g. "23 July, Sunday 12:00"
+  weather: string; // or a custom type if WeatherTypeMap has known keys
+  minTemp: number;
+  maxTemp: number;
+  humidity: string;
+  pressure: string;
+  temp: number;
+  grnd_level: string;
+  water: string;
+  wind: string;
+}
 
 enum WeatherType {
   SUNNY = 'sunny',
   CLOUDY = 'cloudy',
 }
+
+const WeatherTypeMap = {
+  '10n': WeatherType.SUNNY,
+  '10d': WeatherType.SUNNY,
+  '04d': WeatherType.CLOUDY,
+  '04n': WeatherType.CLOUDY,
+  '01n': WeatherType.CLOUDY,
+};
 
 const getWeatherIcon = (size = 24, color = '#fff') => {
   return {
@@ -94,12 +131,82 @@ const sampleData = {
 };
 
 export function WeatherHome() {
+  const [data, setData] = useState<MappedWeatherData>();
+  const [searchCity, setSearchCity] = useState<string>('');
+
+  const getWeatherData = async (city: string = 'Sydney') => {
+    const weatherData = await getCurrentCityForecastData(city).catch((err) => {
+      toast.error('City not found', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        progress: undefined,
+        theme: 'light',
+        transition: Bounce,
+      });
+      return;
+    });
+    if (!weatherData?.data) {
+      return;
+    }
+    const resultData = mapData(weatherData.data);
+    setData(resultData);
+  };
+
+  useEffect(() => {
+    getWeatherData();
+  }, []);
+
+  const mapData = (data: OpenWeatherForecastResponse): MappedWeatherData => {
+    // return 5 days data from now on
+    const filteredData = data.list
+      .filter((item) => {
+        return item.dt_txt.includes('12:00:00');
+      })
+      .map((item: any) => {
+        const date = fromUnixTime(Number(item.dt));
+
+        return {
+          day: format(date, 'EEEE'),
+          date: format(date, 'dd LLLL'),
+          dateLong: format(date, 'hh:mm a'),
+          weather: WeatherTypeMap[item.weather[0].icon],
+          minTemp: parseInt(item.main.temp_min),
+          maxTemp: parseInt(item.main.temp_max),
+          humidity: item.main.humidity,
+          pressure: item.main.pressure,
+          temp: parseInt(item.main.temp),
+          grnd_level: item.main.grnd_level,
+          water: item.main.sea_level,
+          wind: item.wind.speed,
+        };
+      });
+
+    return {
+      currentCity: {
+        city: data.city.name,
+      },
+      forecast: filteredData,
+    };
+  };
+
+  const onSearchCity = () => {
+    console.log('fire');
+    getWeatherData(searchCity);
+  };
+
+  if (!data) {
+    return <>Loading...</>;
+  }
+
   return (
     <main
       className='flex items-center justify-center pt-16 pb-4 text-black'
       style={{
         minHeight: '100vh',
-        backgroundColor: 'purple',
+        backgroundColor: '#624adc',
       }}
     >
       <div
@@ -110,43 +217,37 @@ export function WeatherHome() {
           style={{ backgroundColor: '#4382f7' }}
           className='rounded-xl text-white p-4'
         >
-          <p className='text-xs'>{sampleData.currentCity.date}</p>
-          <p className='text-center text-2xl py-5'>
-            {sampleData.currentCity.city}
-          </p>
-          <p className='text-center text-6xl'>
-            {sampleData.currentCity.maxTemp}
-          </p>
+          <p className='text-xs'>{`${data.forecast[0].date}, ${data.forecast[0].day} ${data.forecast[0].dateLong}`}</p>
+          <p className='text-center text-2xl py-5'>{data.currentCity.city}</p>
+          <p className='text-center text-6xl'>{data.forecast[0].maxTemp}</p>
           <p className='text-center text-xs'>
-            {sampleData.currentCity.minTemp +
-              '~' +
-              sampleData.currentCity.maxTemp}
+            {data.forecast[0].minTemp + '~' + data.forecast[0].maxTemp}
           </p>
           <p className='text-center pt-5 pb-7'>
-            {getWeatherIcon(175)[sampleData.currentCity.weather]}
+            {getWeatherIcon(175)[data.forecast[0].weather]}
           </p>
           <div className='flex justify-center bg-white text-black gap-2 p-3 rounded-lg'>
             <div>
               <WiRaindrop size={50} color={'#000'} />
-              <p>{sampleData.currentCity.water}</p>
+              <p>{data?.forecast[0].water}</p>
             </div>
             <div>
               <WiStrongWind size={50} color={'#000'} />
-              <p>{sampleData.currentCity.wind}</p>
+              <p>{data.forecast[0].wind}</p>
             </div>
             <div>
               <WiDust size={50} color={'#000'} />
-              <p>{sampleData.currentCity.uv}</p>
+              <p>{data.forecast[0].pressure}</p>
             </div>
             <div>
               <WiThermometer size={50} color={'#000'} />
-              <p>{sampleData.currentCity.something}</p>
+              <p>{data.forecast[0].temp}</p>
             </div>
           </div>
         </div>
         <div className='flex flex-col justify-between'>
           <div className='flex justify-between'>
-            {sampleData.forecast.map((item) => {
+            {data?.forecast?.slice(1, 5)?.map((item) => {
               return (
                 <div key={item.date} className='text-center'>
                   <p className='font-bold'>{item.day}</p>
@@ -160,13 +261,19 @@ export function WeatherHome() {
             })}
           </div>
           <div className='mt-30'>
-            <div className='mb-5 relative' style={{ width: '70%' }}>
+            <div className='mb-5 relative' style={{ width: '80%' }}>
               <input
                 type='text'
                 placeholder='Search for a city'
                 className='input-default'
+                value={searchCity}
+                onChange={(e) => setSearchCity(e.target.value)}
               ></input>
-              <button type='button' className='button-default'>
+              <button
+                type='button'
+                className='button-default cursor-pointer me-1.5'
+                onClick={onSearchCity}
+              >
                 Search
               </button>
             </div>
